3cb2ae5fff7cbdaf9b3da03d1e48524c95864516
edakrmm <edakrmm@users.noreply.github.com>
2019-11-14 21:33:49 +0300

    Updates

---
+++ precache-manifest.1ca91d47f163d4b1f0ba3850914616fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd569540777d3e695f73ff157685de34",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "db030a0313823e763c87",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "37a84beee9f5e34beaa5",
+    "url": "/robofriends/static/css/main.feedfdb7.chunk.css"
+  },
+  {
+    "revision": "db030a0313823e763c87",
+    "url": "/robofriends/static/js/2.fbad7c07.chunk.js"
+  },
+  {
+    "revision": "37a84beee9f5e34beaa5",
+    "url": "/robofriends/static/js/main.87215437.chunk.js"
+  },
+  {
+    "revision": "15646e7ae3f7e7d4e926",
+    "url": "/robofriends/static/js/runtime-main.59eca5ac.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
